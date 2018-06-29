1aec7c3a7feebf6822c88b3db49380abcb8f1756
Yuki Toida <yuki.toidax@gmail.com>
2018-06-29 15:47:21 +0900

    Update app.vue

--- assets/src/app.vue
+++ assets/src/app.vue
@@ -3,7 +3,7 @@
     <section class="section">
       <div class="container">
         <h1 class="title">
-          <router-link v-bind:to="{ name: 'Home' }">MP4結合ツール!</router-link>
+          <router-link v-bind:to="{ name: 'Home' }">MP4結合ツール</router-link>
         </h1>
         <h2 class="subtitle">
           MP4ファイルを結合しダウンロードするツール
